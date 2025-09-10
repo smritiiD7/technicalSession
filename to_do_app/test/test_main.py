81bb02a464ae2cec8fea938c1fc41772b66c7477
Smriti <smritidubey66@gmail.com>
2025-09-10 14:39:31 +0530

    Test3

--- to_do_app/test/test_main.py
+++ to_do_app/test/test_main.py
@@ -7,4 +7,4 @@ client  = TestClient(app) #connecting fastapi "app" to the client
 def test_return_health_check():
     response = client.get("/healthy")
     assert response.status_code == status.HTTP_200_OK
-    assert response.json() == {'status': 'Healthy'}
+    assert response.json() == {'status': 'UnHealthy'}
